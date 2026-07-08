9f400dc30577162d75ad0171347700e17ebb69d3
sarora5be25-netizen <sarora5_be25@thapar.edu>
2026-07-08 15:29:02 +0530

    Move Firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,18 @@
 import { initializeApp } from "firebase/app";
-
-import {
-  getAuth,
-  GoogleAuthProvider,
-} from "firebase/auth";
-
-import {
-  getFirestore,
-} from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHcsdHEGCiNEpVrwZIZrC0lnixX6NqOm8",
-  authDomain: "skilltwin-60111.firebaseapp.com",
-  projectId: "skilltwin-60111",
-  storageBucket: "skilltwin-60111.firebasestorage.app",
-  messagingSenderId: "905431248698",
-  appId: "1:905431248698:web:29a57f17ceab3d590dea03",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
 export const provider = new GoogleAuthProvider();
-
 export const db = getFirestore(app);
